0d18f44458f9c9a0c2dbe7e320cd0c88f2b9ffe7
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-09-28 16:24:25 -0400

    commit tree a82fee7cd49829e0288f681358a9d0da40d699d5

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat() {
+    const removeLastCat = cats.slice(0, cats.length - 1);
+    return removeLastCat;
+}
+
+function removeFirstCat() {
+    const removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+}
